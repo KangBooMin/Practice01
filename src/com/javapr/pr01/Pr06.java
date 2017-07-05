c7325f93fa5dbd70f48d0e970a8be284ac077480
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 15:54:24 +0900

    1st

---
+++ src/com/javapr/pr01/Pr06.java
@@ -0,0 +1,33 @@
+package com.javapr.pr01;
+
+import java.util.Scanner;
+
+public class Pr06 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요.");
+		int i = sc.nextInt();
+		int k = 0;
+		int j = 1;
+		int sum = 0;
+		
+			if(i%2==0) {
+				while(k<=i) {
+					sum=sum+k;
+					k=k+2;
+						System.out.println(sum);
+				}
+			} else {
+				while(j<=i) {
+					sum=sum+j;
+					j=j+2;
+						System.out.println(sum);
+				}
+			}
+		
+		sc.close();
+	}
+
+}
