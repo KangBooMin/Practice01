696598559edee09faeb57eeecbd0a392a407548c
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 14:15:25 +0900

    1st

---
+++ src/com/javapr/pr01/Pr04.java
@@ -0,0 +1,14 @@
+package com.javapr.pr01;
+
+public class Pr04 {
+
+	public static void main(String[] args) {
+		
+		for(int i= 1; i<=10; i++) {
+			for(int j= 0; j<=10; j++) {
+				System.out.print(j+i+"\t");		
+			}
+			System.out.println("");
+		}
+	}
+}
