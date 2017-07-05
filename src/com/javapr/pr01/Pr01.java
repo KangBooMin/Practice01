d194927148fb7f151d5237f3cef361093f4b7344
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 12:51:01 +0900

    1st commit

---
+++ src/com/javapr/pr01/Pr01.java
@@ -0,0 +1,10 @@
+package com.javapr.pr01;
+
+public class Pr01 {
+
+	public static void main(String[] args) {
+		System.out.println("aaa");
+
+	}
+
+}
---
+++ src/com/javapr/pr01/Pr02.java
@@ -0,0 +1,10 @@
+package com.javapr.pr01;
+
+public class Pr02 {
+
+	public static void main(String[] args) {
+		System.out.println("bbb");
+
+	}
+
+}
